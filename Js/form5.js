ed8f4c059da849b4272405e4398313c54898e90d
Jhonatan Quirama Pino <50423635+kira4489@users.noreply.github.com>
2021-10-05 18:35:00 -0500

    adding js

---
+++ Js/form5.js
@@ -0,0 +1,10 @@
+function validar_confirmar_correo(string){
+
+}
+
+function validar_pais(string){
+
+}
+
+modules.exports.validar_confirmar_correo = validar_confirmar_correo
+modules.exports.validar_pais = validar_pais
