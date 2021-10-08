6908a0b5ed0e2e1185eb26c35863133a3d3e0666
Jhonatan Quirama Pino <50423635+kira4489@users.noreply.github.com>
2021-10-08 17:34:01 -0500

    javascript validations

--- Js/form5.js
+++ Js/form5.js
@@ -1,7 +1,30 @@
-function validar_confirmar_correo(string){
- string.preventDefault();
- var verificar = document.getElementById("#campoConfirmEmail")
- 
+var email = document.getElementById("campoEmail")
+var confirmCorreo = document.getElementById("campoConfirmarCorreo")
+/* var button = document.getElementById("button") */
+let emailValue = ""
+let confirmEmailValue= ""
+
+/* button.addEventListener("click",()=>{
+ validar_pais()
+}) */
+
+email.addEventListener("change",(event) => {
+  emailValue = event.target.value 
+})
+
+confirmCorreo.addEventListener("change",(event)=>{
+  confirmEmailValue = event.target.value
+  validar_confirmar_correo(emailValue,confirmEmailValue)
+})
+
+
+function validar_confirmar_correo(email,confirmEmail){
+  if(confirmEmail === ""){
+    alert("datos vacios")
+  }
+  if(confirmEmail.length !== email.length){
+    alert("los correos no son iguales")
+  }
 }
 
 function validar_pais(string){
